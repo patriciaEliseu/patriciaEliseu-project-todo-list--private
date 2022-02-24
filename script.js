bb4e7754e3929f0ea5ee25b118d60aa7ae8897f5
Patricia Eliseu <patyeliseu4@gmail.com>
2022-02-23 21:10:24 -0300

    adicionar 7 requisito

--- script.js
+++ script.js
@@ -10,3 +10,36 @@ criarTarefa.addEventListener('click', function() {
     ListaMae.appendChild(novaLi);
     digitarTexto.value = '';
 })
+let butom = document.addEventListener('click', function(event) {
+        let lista = event.target;
+        /*  https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList */
+        lista.classList.toggle("pintar");
+    })
+    /* let lista = document.getElementsByTagName('li');
+
+    novaLi.addEventListener(click, function(event) {
+        event.style.backgroundColor = 'gray';
+
+    lista.addEventListener('click', function() {
+        padding.style.backgroundColor = 'gray';
+    }) */
+    /* 8 - Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo
+    **O que será verificado:**
+    - Será verificado que, quando um elemento da lista é selecionado, o elemento selecionado
+     previamente deixa de sê-lo. Isso é verificado através da presença ou não do estilo 
+     `background-color: gray` no elemento. */
+    /* let itemLista = document.querySelectorAll('li');
+    for (let index = 0; index < itemLista.length; index += 1) {
+        console.log(itemLista[index]);
+    } */
+    /*  let colours = document.getElementsByClassName('color')
+    for (let index = 0; index < colours.length; index += 1) {
+        /* console.log(colours[index]); */
+    /*   colours[index].addEventListener('click', function(event) { */
+    /* console.log(event.target); */
+    /*   let selected = document.getElementsByClassName('selected')[0]; */
+    /* console.log(selected); */
+    /*  selected.classList.remove('selected');
+            event.target.classList.add('selected');
+        })
+    } */
